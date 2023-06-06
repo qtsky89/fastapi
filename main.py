01aaa4dc7b3ad66bfb16a9053e2a913d095d4dff
Wonhee Jang <Qtsky89@gmail.com>
2023-06-06 15:32:14 +0900

    [#1] Added query param feature

--- main.py
+++ main.py
@@ -50,5 +50,13 @@ def update_item(item_id: int, item: Item):
 
 
 @app.get("/files/{fp:path}")
-async def read_file(fp: str):
+def read_file(fp: str):
     return {"file_path": fp}
+
+
+fake_item_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+def read_item(skip: int = 0, limit: int = 10):
+    return fake_item_db[skip: skip + limit]
