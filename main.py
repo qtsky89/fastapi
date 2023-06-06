9caa0c50ffef6f273bb6a1b9e5fddcc689276b66
Wonhee Jang <Qtsky89@gmail.com>
2023-06-06 15:27:04 +0900

    [#1] Added file path

--- main.py
+++ main.py
@@ -47,3 +47,8 @@ def read_item(item_id: int):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/files/{fp:path}")
+async def read_file(fp: str):
+    return {"file_path": fp}
