cfd8051e1d68404b182c191f96872a250ca46227
Wonhee Jang <Qtsky89@gmail.com>
2023-06-06 20:49:33 +0900

    [#2] Added alias

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ async def read_items(
             title="Query string",
             description="Query string for the items to search in the database that have a good match",
             min_length=3,
+            alias="item-query"
         ),
     ] = None
 ):
